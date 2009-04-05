df450db6569851b56e24e7148a6fed3314a73f77
Nicholas Cancelliere <ncancelliere@gmail.com>
2009-04-04 20:57:14 -0500

    added gems to env config files

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'RedCloth', :lib => 'redcloth'
+  config.gem 'fastercsv', :lib => 'fastercsv'
+  config.gem 'haml', :lib => 'haml'
+  config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'thoughtbot-clearance', :lib => 'clearance', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

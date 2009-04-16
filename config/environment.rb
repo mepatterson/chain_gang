5f378ff347d1c69fe43da6ccad38bd7dec881eed
Matt Patterson <mpatterson@Matt-Pattersons-iMac.local>
2009-04-16 13:38:00 -0500

    adds calendar_date_select gem

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-clearance', :lib => 'clearance', :source => 'http://gems.github.com'
   config.gem 'capistrano', :lib => false
   config.gem 'capistrano-ext', :lib => false
+  config.gem "calendar_date_select"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
